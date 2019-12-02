dadbe720d7c4f6f290e7df8f044e7e301bf856b5
BaronLip <baronlip@gmail.com>
2019-12-02 14:36:37 -0600

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-// add any needed imports here
+import { connect } from 'react-redux';
+// import UserInput from './UserInput';
+
 class Users extends Component {
 
-  render() {
-    return (
-      <div>
-        <ul>
-          Users!
-          {/* Write code here that displays the usernames of all users in the Redux store */}
-          {/* In addition, display the total number of users curently in the store */}
-        </ul>
-      </div>
-    )
-  }
+    render() {
+        console.log(this.props.users)            
+        const users = this.props.users.map((user, index) => {
+            return <li>{index + 1 }. {user.username}</li>
+        })
+
+
+        return (
+        <div>
+            <ul>
+                Users!
+                {/* Write code here that displays the usernames of all users in the Redux store */ 
+                }
+                {users}      
+            
+                {/* In addition, display the total number of users currently in the store */}
+            </ul>
+        </div>
+        )
+    }
 }
 
 //add mapStateToProps here
+const mapStateToProps = (state) => {
+    return { users: state.users }
+}
 
 // connect this component to Redux
-export default Users
+export default connect(mapStateToProps)(Users);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
+// Import createStore to have the template store with getState and dispatch function.
+import {createStore} from 'redux'
+// Import Provider in order to provide App with store functionality.
+import {Provider} from 'react-redux'
+// Import manageUsers reducer from reducers folder.
 import manageUsers from './reducers/manageUsers'
 
-
-// add imports and code
+// Pass manageUsers reducer to the createStore function. Assign it to "store".
+const store = createStore(manageUsers)
 
 
 ReactDOM.render(
-  // add imports and code
-  <App />
-  // add imports and code
-  ,
-  document.getElementById('root')
+    // Wrap App with Provider and create props of "store" to be "provided" to App.
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
